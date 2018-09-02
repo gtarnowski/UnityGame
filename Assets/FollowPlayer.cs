4ac81fea79a9651bcb5e351bae1bf0146a4042ed
Greg Tarnowski <greg.tarnowski@ncgroup.co>
2018-09-02 20:24:15 +0200

    Inital commit

---
+++ Assets/FollowPlayer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour {
+
+	public Transform PlayerTransform;
+	private Vector3 _cameraOffset;
+
+	[Range(0.01f, 1.0f)]
+	public float SmoothFactor = 0.5f;
+	public bool LookAtPlayer = false;
+	public bool RotateArroundPlayer = false;
+	public float RotationSpeed = 5.0f;
+
+	// Use this for initialization
+	void Start () {
+		_cameraOffset = transform.position - PlayerTransform.position;	
+	}
+	
+	// Update is called after Update methods
+	void LateUpdate () {
+		if (RotateArroundPlayer && Input.GetMouseButton(0)) {
+			Quaternion camTurnAngle = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * RotationSpeed, Vector3.up);
+			_cameraOffset = camTurnAngle * _cameraOffset;
+		}
+
+		Vector3 newPos = PlayerTransform.position + _cameraOffset;
+		transform.position = Vector3.Slerp(transform.position, newPos, SmoothFactor);
+
+		if (LookAtPlayer || RotateArroundPlayer) {
+			transform.LookAt(PlayerTransform);
+		} 
+	}
+}
---
+++ Assets/PlayerMove.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour {
+	public float moveSpeed;
+	public float jumpHeight;
+	private Rigidbody rigidBody;
+
+	void Start () {
+		moveSpeed = 3f;
+		rigidBody = GetComponent<Rigidbody>();
+	}
+	void FixedUpdate() {
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			rigidBody.velocity = new Vector3(0, 10 * jumpHeight * Time.deltaTime, 0);
+		}	
+	}
+	void Update () {
+		transform.Translate(
+			moveSpeed * Input.GetAxis("Horizontal") * Time.deltaTime,
+			0f,
+			moveSpeed * Input.GetAxis("Vertical") * Time.deltaTime
+		);	
+	}
+}
